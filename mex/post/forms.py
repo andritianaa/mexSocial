a8017e78438296f4ceb832823b5f4ad6abdcdcdd
Rakotonimanana T. Andritiana Steve <andritianasteve@gmail.com>
2021-08-31 21:50:26 +0300

    create new post terminée

---
+++ mex/post/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from django.forms import fields
+from post.models import Post
+from confidentialite.models import Confidentialite
+
+class NewPostForm(forms.ModelForm):
+    title = forms.CharField(widget=forms.TextInput(attrs={'class' : 'validate'}), required=True)
+    content = forms.FileField(widget=forms.ChoiceWidget(attrs={'multiple' : True}), required=True)
+    description = forms.CharField(widget=forms.Textarea(attrs={'class' : 'materialize-textarea'}), required=True)
+    confidentialite = forms.ModelChoiceField(queryset=Confidentialite.objects.all())
+    
+    class Meta:
+        model= Post
+        fields = ('content','title','description','confidentialite')
