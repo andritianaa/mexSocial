8bed587fb9c4060a5dd3656e829fe5c2cf1a37a6
Rakotonimanana T. Andritiana Steve <andritianasteve@gmail.com>
2021-08-31 16:34:10 +0300

    création nouvelle confidentialité terminée

---
+++ mex/confidentialite/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from django.forms import fields, models
+from confidentialite.models import Confidentialite
+
+class nouvelConfidentialiteForm(forms.ModelForm):
+    description = forms.CharField(widget=forms.TextInput(attrs={'class' : 'zone-de-texte'}), required=True)
+    can_message = forms.BooleanField(required=False)
+    
+    class Meta:
+        model = Confidentialite
+        fields = ('description', 'can_message')
