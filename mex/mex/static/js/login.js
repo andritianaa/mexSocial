ebce594de8aab4e6db75b0d727acf78c8821a65c
Rakotonimanana T. Andritiana Steve <andritianasteve@gmail.com>
2021-10-31 13:09:12 +0300

    login style done

---
+++ mex/mex/static/js/login.js
@@ -0,0 +1,19 @@
+window.addEventListener('load', function myfct(e) {
+    const inputs = document.querySelectorAll('.input_login');
+    function focusFunc() {
+        let parent = this.parentNode.parentNode;
+        parent.classList.add('focus');
+    }
+    function blurFunc() {
+        let parent = this.parentNode.parentNode;
+        if (this.value == "") {
+            parent.classList.remove('focus');
+        }
+    }
+    inputs.forEach(input => {
+        
+        input.addEventListener('focus', focusFunc);
+        input.addEventListener('blur', blurFunc);
+    });
+
+})
